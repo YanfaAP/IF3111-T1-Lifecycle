1f490854bd840f977ec69fd141dbe2d3927bfc58
Yanfa Adi Putra <yanfa.adiputra@gmail.com>
2015-02-08 02:35:30 +0700

    commit

---
+++ AndroidIntents/AndroidBrowser/app/src/main/java/com/yanfa/androidbrowser/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yanfa.androidbrowser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button browserButton = (Button)findViewById(R.id.browserButton);
+        browserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vogella.com"));
+                startActivity(intent);
+            }
+        });
+    }
+
+}
